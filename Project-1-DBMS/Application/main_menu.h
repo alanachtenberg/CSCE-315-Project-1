319005644bec00f4a27b399292905d3cc90413b6
jacob.stone56 <jacob.stone56@tamu.edu>
2014-10-01 17:29:38 -0500

    expanded interface  in main_menu.cpp

--- Project-1-DBMS/Application/main_menu.h
+++ Project-1-DBMS/Application/main_menu.h
@@ -5,10 +5,10 @@
 
 int check_input(int input, int low, int high);
 void main_menu();
-void address_book();
-void calendar();
-void Memo();
-void Todo();
+void Address_Book();
+void Calendar();
+void Memo_Pad();
+void Todo_List();
 
 
 #endif // MAIN_MENU_H
