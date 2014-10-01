94b69674cccc260b18560da9120ad31e5cf80d42
jacob.stone56 <jacob.stone56@tamu.edu>
2014-10-01 16:55:07 -0500

    added main_menu h file

---
+++ Project-1-DBMS/Application/main_menu.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+
+
+int check_input(int input, int low, int high);
+void main_menu();
+void address_book();
+void calendar();
+void Memo();
+void Todo();
+
+
+#endif // MAIN_MENU_H
