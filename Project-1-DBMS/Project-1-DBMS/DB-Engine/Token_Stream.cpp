0dbb954a3063d4b8265814515768ff89149df940
Casey Brooks <cjbrooks12@tamu.edu>
2014-10-05 15:47:06 -0500

    improved tokenizer: better literals, signed numbers, case sensitivity

--- Project-1-DBMS/Project-1-DBMS/DB-Engine/Token_Stream.cpp
+++ Project-1-DBMS/Project-1-DBMS/DB-Engine/Token_Stream.cpp
@@ -1,6 +1,7 @@
 #include "Token_Stream.h"
 #include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -14,6 +15,7 @@ Token Token_stream::get() {
 	Token curr_token(_null);
 	char ch;
 	string s;
+	bool negative = false;
 
 	if (!unget_tokens.empty()) {
 		curr_token = unget_tokens.top();
@@ -27,13 +29,50 @@ Token Token_stream::get() {
 		case '(': curr_token = Token(_lpar); break;
 		case ')': curr_token = Token(_rpar); break;
 		case '+': curr_token = Token(_plus); break;
-		case '-': curr_token = Token(_minus); break;
+		case '-': 
+			//char following minus is a number, so we are reading a negative number
+			if (isdigit(input_stream->peek())) {
+				s = "";
+				s += ch;
+				while (input_stream->get(ch) && (isdigit(ch) || ch == '.')) {
+					s += ch;
+				}
+
+				input_stream->unget();
+				stringstream ss(s);
+				//determine if the number is a float or integer
+				if (s.find('.') == string::npos) {
+					int int_val;
+					ss >> int_val;
+					curr_token = Token(_int_num, int_val);
+				}
+				else {
+					float float_val;
+					ss >> float_val;
+					curr_token = Token(_float_num, float_val);
+				}
+				break;
+			}
+			//if we don't have a negative number, then its the operator
+			else {
+				curr_token = Token(_minus); 
+				break;
+			}
 		case '*': curr_token = Token(_multiply); break;
 		case '/': curr_token = Token(_divide); break;
 		case '%': curr_token = Token(_mod); break;
 		case ';': curr_token = Token(_semicolon); break;
 		case ',': curr_token = Token(_comma); break;
-		case '"': curr_token = Token(_quotation); break;
+		case '"': 
+			getline(*input_stream, s, '\"');
+			if (input_stream->eof()) {
+				BadTokenException ex("Bad Token: missing end quotation in literal");
+				throw ex;
+			}
+			else {
+				curr_token = Token(_literal, s);
+				break;
+			}
 		case '<':
 			input_stream->get(ch);
 			if (ch == '-') {
@@ -122,34 +161,36 @@ Token Token_stream::get() {
 					s += ch;
 				}
 				input_stream->unget();
-				transform(s.begin(), s.end(), s.begin(), ::tolower); //convert string to lowercase
+				//transform(s.begin(), s.end(), s.begin(), ::tolower); //convert string to lowercase
 
 				//reserved words
-				if (s == "create")		curr_token = Token(_create);
-				else if (s == "insert")		curr_token = Token(_insert);
+				if (s == "CREATE")			curr_token = Token(_create);
+				else if (s == "INSERT")		curr_token = Token(_insert);
 				else if (s == "select")		curr_token = Token(_select);
 				else if (s == "project")	curr_token = Token(_project);
-				else if (s == "show")		curr_token = Token(_show);
+				else if (s == "SHOW")		curr_token = Token(_show);
 				else if (s == "rename")		curr_token = Token(_rename);
 
-				else if (s == "into")		curr_token = Token(_into);
-				else if (s == "table")		curr_token = Token(_table);
-				else if (s == "values")		curr_token = Token(_values);
-				else if (s == "from")		curr_token = Token(_from);
-				else if (s == "primary")	curr_token = Token(_primary);
-				else if (s == "key")		curr_token = Token(_key);
+				else if (s == "INTO")		curr_token = Token(_into);
+				else if (s == "TABLE")		curr_token = Token(_table);
+				else if (s == "VALUES")		curr_token = Token(_values);
+				else if (s == "FROM")		curr_token = Token(_from);
+				else if (s == "PRIMARY")	curr_token = Token(_primary);
+				else if (s == "KEY")		curr_token = Token(_key);
 
-				else if (s == "relation")	curr_token = Token(_relation);
-				else if (s == "write")		curr_token = Token(_write);
-				else if (s == "open")		curr_token = Token(_open);
-				else if (s == "close")		curr_token = Token(_close);
-				else if (s == "delete")		curr_token = Token(_delete);
-				else if (s == "update")		curr_token = Token(_update);
-				else if (s == "set")		curr_token = Token(_set);
-				else if (s == "where")		curr_token = Token(_where);
+				else if (s == "RELATION")	curr_token = Token(_relation);
+				else if (s == "WRITE")		curr_token = Token(_write);
+				else if (s == "OPEN")		curr_token = Token(_open);
+				else if (s == "CLOSE")		curr_token = Token(_close);
+				else if (s == "DELTE")		curr_token = Token(_delete);
+				else if (s == "UPDATE")		curr_token = Token(_update);
+				else if (s == "SET")		curr_token = Token(_set);
+				else if (s == "WHERE")		curr_token = Token(_where);
 
-				else if (s == "integer")	curr_token = Token(_int_type);
-				else if (s == "varchar") {
+				else if (s == "FLOAT")		curr_token = Token(_float_type);
+				else if (s == "INTEGER")	curr_token = Token(_int_type);
+				else if (s == "CHAR")		curr_token = Token(_char);
+				else if (s == "VARCHAR") {
 					char lpar, rpar;
 					int size;
 					*input_stream >> lpar;
@@ -160,10 +201,7 @@ Token Token_stream::get() {
 						}
 					}
 				}
-				else if (s == "char")		curr_token = Token(_char);
-				else if (s == "float")		curr_token = Token(_float_type);
-
-				else if (s == "exit")		curr_token = Token(_exit_program);
+				else if (s == "EXIT")		curr_token = Token(_exit_program);
 				else						curr_token = Token(_identifier, s);
 				break;
 			}
@@ -181,26 +219,4 @@ Token Token_stream::get() {
 		BadTokenException ex("Attempt to read from empty stream");
 		throw ex;
 	}
-}
-
-vector<Token> Token_stream::tokenize() {
-	Token t = get();
-
-	while (true) {
-		if (t.get_type() == _exit_program) {
-			break;
-		}
-		t = get();
-	}
-
-	return tokens;
-}
-
-//Allows for a bad token to be ignored and not interfere with execution
-void Token_stream::ignore(Token_Type c) {
-	//if ( == unget_tokens.top().get_type()) {
-		//return;
-	//}
-
-	return;
 }
--- Project-1-DBMS/Project-1-DBMS/DB-Engine/main.cpp
+++ Project-1-DBMS/Project-1-DBMS/DB-Engine/main.cpp
@@ -11,4 +11,20 @@ void test_parser() {
 int main(){
 	DBParser parser;
 	parser.execute_file("final_test.txt");
+	/*try {
+		Token_stream ts("select -2.1 \"this is an example of a literal\"");
+		Token token;
+		token = ts.get();
+		cout << token.get_type_name() << " " << token.get_value() << " " << token.get_name() << "\n";
+
+		token = ts.get();
+		cout << token.get_type_name() << " " << token.get_value() << " " << token.get_name() << "\n";
+
+		token = ts.get();
+		cout << token.get_type_name() << " " << token.get_value() << " " << token.get_name() << "\n";
+	}
+	catch (BadTokenException e) {
+		cout << e.what();
+	}
+	system("pause");*/
 }
